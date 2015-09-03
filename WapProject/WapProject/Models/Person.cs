f19aec28ceb9649f5192f43a806f6ed454d0ff77
unknown <sayedha@sih-x3.redmond.corp.microsoft.com>
2015-09-03 11:49:53 -0700

    adding projct

---
+++ WapProject/WapProject/Models/Person.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WapProject.Models
+{
+    public class Person
+    {
+        public int ID { get; set; }
+
+        [Required]
+        [MaxLength(25)]
+        public string FirstName { get; set; }
+
+        [MaxLength(25)]
+        public string LastName { get; set; }
+
+        public string NickName { get; set;}
+    }
+}
