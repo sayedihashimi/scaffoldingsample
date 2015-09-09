c601b08748985974fdb7bed980dc9d851cd210f0
Sayed Ibrahim Hashimi <sayed.hashimi@gmail.com>
2015-09-09 11:00:41 -0700

    adding dnx project

---
+++ dnxproject/MyDnxWeb/src/MyDnxWeb/Models/Person.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+
+namespace MyDnxWeb.Models
+{
+    public class Person
+    {
+        public int ID { get; set; }
+
+        [Required]
+        [MaxLength(25)]
+        public string FirstName { get; set; }
+
+        [MaxLength(25)]
+        public string LastName { get; set; }
+
+        public string NickName { get; set; }
+    }
+}
+
+
